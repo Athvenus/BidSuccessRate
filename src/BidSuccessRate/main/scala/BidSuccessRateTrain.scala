58dc108da89d61bc49eb28e460464c81c006b669
baijinmei <baijinmei@ycmedia.cn>
2016-05-23 13:56:34 +0800

    add main

--- src/BidSuccessRate/main/scala/BidSuccessRateTrain.scala
+++ src/BidSuccessRate/main/scala/BidSuccessRateTrain.scala
@@ -1,6 +1,7 @@
 package BidSuccessRate.main.scala
 
 
+import BidSuccessRate._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
@@ -10,7 +11,10 @@ object BidSuccessRateTrain {
   def main(args:Array[String]) {
     val sc=new SparkContext()
     val documentPath = ""
-    
-    
+    val data = sc.textFile(documentPath)
+    val trainer = new SimpleNuralNetworkTrainer(sc,data,10,2,1000000,0.01,0.01,10000,0.005)
+    //sc:SparkContext,data:RDD[String],iterations:10,hiddenum:2,binarylen:1000000,initialWeight:0.01,rate:0.01,batchSize:10000,convergence:0.005
+    val model = trainer.model   
+    println("Model Training Finished.")
   }
 }
