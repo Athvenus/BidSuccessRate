c77193f0d75bb4ced88b626c618aae32405dda99
baijinmei <baijinmei@ycmedia.cn>
2016-05-25 09:48:56 +0800

    change data

--- src/main/scala/cn/ycmedia/BidSuccessRate/BidSuccessRateTrain.scala
+++ src/main/scala/cn/ycmedia/BidSuccessRate/BidSuccessRateTrain.scala
@@ -8,7 +8,7 @@ import org.apache.spark.SparkContext
 object BidSuccessRateTrain {
   def main(args:Array[String]) {
     val sc=new SparkContext()
-    val documentPath = "/opt/dmp/mspace/bsr/160517"
+    val documentPath = "/opt/dmp/mspace/bsr/160522"
     val data = sc.textFile(documentPath)
     val trainer = new SimpleNuralNetworkTrainer(sc,data,10,2,1000000,0.01,0.01,10000,0.005)
     //sc:SparkContext,data:RDD[String],iterations:10,hiddenum:2,binarylen:1000000,initialWeight:0.01,rate:0.01,batchSize:10000,convergence:0.005
