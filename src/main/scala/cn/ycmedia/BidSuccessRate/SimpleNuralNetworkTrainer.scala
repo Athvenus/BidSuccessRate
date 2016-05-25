d91bc91203d0fded7a4c7f02684c2f27007aec22
baijinmei <baijinmei@ycmedia.cn>
2016-05-25 13:51:25 +0800

    update weight length

--- src/main/scala/cn/ycmedia/BidSuccessRate/SimpleNuralNetworkTrainer.scala
+++ src/main/scala/cn/ycmedia/BidSuccessRate/SimpleNuralNetworkTrainer.scala
@@ -54,7 +54,7 @@ class SimpleNuralNetworkTrainer (sc:SparkContext,data:RDD[String],iterations:Int
     
     var w0 = Weight.value 
     
-    println("In this iteration,start weight is ",w0,"Predicts is",w0(0)(0),"Hidden Weight is",w0(4)(0),w0(4)(1),w0(4)(2),w0(4)(3))
+    println("In this iteration,start weight is ",w0)
     
     def updateBatchModel(iter:Iterator[Array[Array[Double]]]):Iterator[Seq[Array[Array[Double]]]] = {
       //Update weight,predict and hidden example values
