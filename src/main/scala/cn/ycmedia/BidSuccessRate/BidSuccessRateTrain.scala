fde86df3fd99ec06e2f9a2764f450e2a313b1e05
baijinmei <baijinmei@ycmedia.cn>
2016-05-26 11:08:24 +0800

    less more data

--- src/main/scala/cn/ycmedia/BidSuccessRate/BidSuccessRateTrain.scala
+++ src/main/scala/cn/ycmedia/BidSuccessRate/BidSuccessRateTrain.scala
@@ -9,8 +9,8 @@ object BidSuccessRateTrain {
   def main(args:Array[String]) {
     val conf = new SparkConf().set("spark.driver.maxResultSize","5G")
     val sc=new SparkContext(conf)
-    val documentPath = "/opt/dmp/mspace/bsr/160522"
-    val data = sc.textFile(documentPath).randomSplit(Array(0.05, 0.95), seed = 11L)
+    val documentPath = "/opt/dmp/mspace/bsr/160522/part-00000"
+    val data = sc.textFile(documentPath).randomSplit(Array(0.5, 0.5), seed = 11L)
     val training = data(0)
     val trainer = new SimpleNuralNetworkTrainer(sc,training,10,2,500000,0.01,0.01,50000,0.005)
     //sc:SparkContext,data:RDD[String],iterations:10,hiddenum:2,binarylen:1000000,initialWeight:0.01,rate:0.01,batchSize:10000,convergence:0.005
