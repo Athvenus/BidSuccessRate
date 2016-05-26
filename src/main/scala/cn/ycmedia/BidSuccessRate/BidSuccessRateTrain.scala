e1d21554e27b3271bd851aa66e7d17162827dd73
baijinmei <baijinmei@ycmedia.cn>
2016-05-26 11:13:54 +0800

    tuning partition number

--- src/main/scala/cn/ycmedia/BidSuccessRate/BidSuccessRateTrain.scala
+++ src/main/scala/cn/ycmedia/BidSuccessRate/BidSuccessRateTrain.scala
@@ -12,7 +12,7 @@ object BidSuccessRateTrain {
     val documentPath = "/opt/dmp/mspace/bsr/160522/part-00000"
     val data = sc.textFile(documentPath).randomSplit(Array(0.5, 0.5), seed = 11L)
     val training = data(0)
-    val trainer = new SimpleNuralNetworkTrainer(sc,training,10,2,500000,0.01,0.01,50000,0.005)
+    val trainer = new SimpleNuralNetworkTrainer(sc,training,10,2,500000,0.01,0.01,5000,0.005)
     //sc:SparkContext,data:RDD[String],iterations:10,hiddenum:2,binarylen:1000000,initialWeight:0.01,rate:0.01,batchSize:10000,convergence:0.005
     val model = trainer.model   
     println("Model Training Finished.model ",model)
