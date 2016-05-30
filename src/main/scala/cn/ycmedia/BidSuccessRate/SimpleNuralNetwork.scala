ec5d4009185ee0e3c87ad487dccafbc5e93fba95
baijinmei <baijinmei@ycmedia.cn>
2016-05-30 12:48:08 +0800

    modify predictor and update logic

--- src/main/scala/cn/ycmedia/BidSuccessRate/SimpleNuralNetwork.scala
+++ src/main/scala/cn/ycmedia/BidSuccessRate/SimpleNuralNetwork.scala
@@ -9,7 +9,6 @@ class SimpleNuralNetwork (binarynum:Int,binarylen:Int,rate:Double) extends Seria
     
   class predictor(weight:Array[Array[Double]],example:Array[Array[Double]]) extends Serializable{
     //Output Layer Predictor
-    var predictedvalue = 0.0
     val simi_example = new Array[Array[Double]](example.length)
     for(i <- Iterator.range(0,example.length)){
       simi_example.update(i,new Array[Double](example(i).length))
@@ -19,10 +18,10 @@ class SimpleNuralNetwork (binarynum:Int,binarylen:Int,rate:Double) extends Seria
       //Signs Come From Input Array
       val perceivedvalue = new LinearPerceptron (weight(i),example(i),"sigmoid").perception
       simi_example(binarynum+2).update(i-1,perceivedvalue)
-      predictedvalue += outputweight*perceivedvalue
  
     }
     def predict:Double = {
+      val predictedvalue = new LinearPerceptron(weight(binarynum+2),simi_example(binarynum+2),"sigmoid").squash
       weight(0).update(0,predictedvalue)
       predictedvalue
     }
@@ -40,8 +39,8 @@ class SimpleNuralNetwork (binarynum:Int,binarylen:Int,rate:Double) extends Seria
  
   
   def hiddenloss(weight:Array[Array[Double]],example:Array[Array[Double]]):Array[Double] = {
-    val hiddenloss = new Array[Double](binarynum)
-    for{i <- Iterator.range(0,binarynum)}{
+    val hiddenloss = new Array[Double](binarynum+1)
+    for{i <- Iterator.range(0,binarynum+1)}{
       val perceivedvalue = new LinearPerceptron (weight(i+1),example(i+1),"sigmoid").perception
       val oloss = outputloss(weight,example)
       val hloss = perceivedvalue*(1-perceivedvalue)*(weight(binarynum+1)(i)*oloss)
@@ -52,20 +51,31 @@ class SimpleNuralNetwork (binarynum:Int,binarylen:Int,rate:Double) extends Seria
   
   
   def update(weight:Array[Array[Double]],example:Array[Array[Double]]): Array[Array[Double]] = {
-    val updated = new Array[Array[Double]](example.length)
-    for(i <- Iterator.range(0,example.length)){
-      updated.update(i,new Array[Double](example(i).length))
+    val updated = new Array[Array[Double]](weight.length)
+    for(i <- Iterator.range(0,weight.length)){
+      updated.update(i,new Array[Double](weight(i).length))
     }
-    for{i <- Iterator.range(1,binarynum+1)}{
+    for{i <- Iterator.range(1,weight.length)}{
       for{j <- Iterator.range(0,weight(i).length)}{
-        var w_ij=weight(i)(j);
-        var delta_w_ij=0
-        if(i < binarynum+1){
-          var delta_w_ij = hiddenloss(weight,example)
+        var w_ij=weight(i)(j)
+        val delta_w_ij=0.0
+        if(i < weight.length-1){
+          //update hidden weight
+          val delta_w_ij = hiddenloss(weight,example)(i-1)
+          w_ij+=rate*delta_w_ij*example(i)(j)
         }else{
-          var delta_w_ij = outputloss(weight,example)
+          //update output weight
+          val delta_w_ij = outputloss(weight,example)
+          w_ij+=rate*delta_w_ij*example(i)(j)
         }
-        w_ij+=rate*delta_w_ij*example(i)(j)
+        if(delta_w_ij > 0){
+          println("delta_w_ij is not zero",delta_w_ij)
+        }
+        if(example(i)(j) > 0){
+          //println("example is",example(i)(j))
+          //println("delta_w_ij is",delta_w_ij)
+          //println("w_ij is ",w_ij)
+        }
         updated(i).update(j,w_ij)
       }
     } 
